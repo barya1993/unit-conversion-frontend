5a75a070c9d1984a48b6b194e427b0391d996864
Nitish Barya <barya1993@gmail.com>
2022-11-13 22:59:42 -0500

    feat(temp-convertor): add service for temperature unit conversion

--- src/App.js
+++ src/App.js
@@ -4,8 +4,52 @@ import { ReactComponent as KelvinLogo } from './assets/kelvin-logo.svg';
 import { ReactComponent as FahrenheitLogo } from './assets/fahrenheit-logo.svg';
 import { ReactComponent as RankineLogo } from './assets/rankine-logo.svg';
 import './App.css';
+import { convertTemperature } from './services/TempratureService';
 
 function App() {
+  const tempratureUnits = ['Kelvin', 'Celcius', 'Fahrenheit', 'Rankine'];
+
+  const [tempIn, setTempIn] = useState('Kelvin');
+  const [tempOut, setTempOut] = useState('Kelvin');
+  const [valueIn, setValueIn] = useState('');
+  const [valueOut, setValueOut] = useState('');
+  const [answer, setAnswer] = useState('');
+
+  const isSubmitEnabled = valueIn.length > 0 && valueOut.length > 0;
+
+  const onChangeValueIn = (event) => {
+    const value = event.currentTarget.value;
+    setValueIn(value);
+  };
+
+  const handleTempInChange = (event) => {
+    const value = event.target.value;
+    setTempIn(value);
+  };
+
+  const handleTempOutChange = (event) => {
+    const value = event.target.value;
+    setTempOut(value);
+  };
+
+  const onChangeValueOut = (event) => {
+    const value = event.currentTarget.value;
+    setValueOut(value);
+  };
+
+  const onSubmitForm = (event) => {
+    if (event !== undefined && event.preventDefault) {
+      event.preventDefault();
+    }
+    const convertedTemp = convertTemperature(tempIn, tempOut, valueIn);
+    console.log(convertedTemp, typeof valueOut, parseFloat(valueOut, 10));
+    if (parseFloat(valueOut, 10) === parseFloat(convertedTemp, 10)) {
+      setAnswer('Correct');
+    } else {
+      setAnswer('Incorrect');
+    }
+  };
+
   return (
     <div className="app">
       <div className="page-wrapper">
@@ -19,6 +63,71 @@ function App() {
           <header>
             <h1 className="title">Temperature Unit Conversion</h1>
           </header>
+        </div>
+        <p className="subtitle">Enter value, unit, conversion unit and output value:</p>
+        <div className="form-container">
+          <form className="form" onSubmit={onSubmitForm}>
+            <label className="form-label">Temprature Value</label>
+            <div className="text-input-container">
+              <input
+                aria-label="temp-input"
+                className="input"
+                type="text"
+                placeholder="Enter temperature value"
+                value={valueIn}
+                onChange={onChangeValueIn}
+              />
+            </div>
+            <label className="form-label">Temprature unit</label>
+            <div className="radio-container">
+              {tempratureUnits.map((tempratureUnit) => (
+                <div className="radio" key={`tempIn${tempratureUnit}`}>
+                  <input
+                    type="radio"
+                    id={`tempIn${tempratureUnit}`}
+                    onChange={handleTempInChange}
+                    value={tempratureUnit}
+                    checked={tempIn === tempratureUnit}
+                    data-testid={`tempIn${tempratureUnit}`}
+                  />
+                  <label htmlFor={`tempIn${tempratureUnit}`}>{tempratureUnit}</label>
+                </div>
+              ))}
+            </div>
+            <label className="form-label">Conversion Unit</label>
+            <div className="radio-container">
+              {tempratureUnits.map((tempratureUnit) => (
+                <div className="radio" key={`tempOut${tempratureUnit}`}>
+                  <input
+                    type="radio"
+                    id={`tempOut${tempratureUnit}`}
+                    onChange={handleTempOutChange}
+                    value={tempratureUnit}
+                    checked={tempOut === tempratureUnit}
+                    data-testid={`tempOut${tempratureUnit}`}
+                  />
+                  <label htmlFor={`tempOut${tempratureUnit}`}>{tempratureUnit}</label>
+                </div>
+              ))}
+            </div>
+            <label className="form-label">Converted Value</label>
+            <div className="text-input-container">
+              <input
+                aria-label="temp-output"
+                className="input"
+                type="text"
+                placeholder="Enter temperature value"
+                value={valueOut}
+                onChange={onChangeValueOut}
+              />
+            </div>
+            <div>
+              <input disabled={!isSubmitEnabled} className="input" type="submit" value="Submit" />
+            </div>
+          </form>
+        </div>
+        <div data-testid="answer">
+          {answer !== '' ? <div data-testid="answer">{answer}</div> : <></>}
         </div>
       </div>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,61 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('./services/TempratureService', () => ({
+  convertTemperature: (from, to, input) => '100'
+}));
+
+describe('<App />', () => {
+  it('Renders <App /> component correctly', () => {
+    const { getByText } = render(<App />);
+    expect(screen.getByText(/Temperature Unit Conversion/i)).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toHaveTextContent(/Temperature Unit Conversion/);
+  });
+
+  it('Test input change correctly', () => {
+    const { getByLabelText } = render(<App />);
+    const input = getByLabelText('temp-input');
+    fireEvent.change(input, { target: { value: '23' } });
+    expect(input.value).toBe('23');
+  });
+
+  it('Test output change correctly', () => {
+    const { getByLabelText } = render(<App />);
+    const input = getByLabelText('temp-output');
+    fireEvent.change(input, { target: { value: '100' } });
+    expect(input.value).toBe('100');
+  });
+
+  it('Test input radio button correctly', () => {
+    const { getByTestId } = render(<App />);
+    const radio = getByTestId('tempInRankine');
+    expect(radio.checked).toEqual(false);
+    fireEvent.click(radio);
+    expect(radio.checked).toEqual(true);
+  });
+
+  it('Test output radio button correctly', () => {
+    const { getByTestId } = render(<App />);
+    const radio = getByTestId('tempOutKelvin');
+    expect(radio.checked).toEqual(true);
+  });
+
+  it('Test submit button disable correctly', () => {
+    const { getByTestId, getByRole, getByLabelText } = render(<App />);
+    const input = getByLabelText('temp-input');
+    const output = getByLabelText('temp-output');
+    const button = getByRole('button', { name: 'Submit' });
+    const answer = getByTestId('answer');
+
+    expect(button).toBeDisabled();
+    fireEvent.change(input, { target: { value: '100' } });
+    fireEvent.change(output, { target: { value: '100' } });
+    expect(button).toBeEnabled();
+    fireEvent.click(button);
+    expect(answer).toBeInTheDocument();
+    fireEvent.change(input, { target: { value: '0' } });
+    fireEvent.change(output, { target: { value: '100' } });
+    fireEvent.click(button);
+    expect(answer).toBeInTheDocument();
+  });
 });
